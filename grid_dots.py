9779f24f21526426860e1600c5865798325cff74
carlynorama <carlynorama@gmail.com>
2020-04-25 20:07:54 -0700

    use inches

--- grid_dots.py
+++ grid_dots.py
@@ -1,6 +1,7 @@
-centerX = 10
-centerY = 10
-radius = 3
+centerX = 0.25
+centerY = 0.25
+radius = 0.25
+margin = 0.25
 style = "fill:rgb(200,200,255);"
 
 f = open('helloworld.svg','w')
@@ -9,10 +10,10 @@ f.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graph
 f.write('<svg width="8.5in" height="11in" xmlns="http://www.w3.org/2000/svg">\n')
 
 for y in range(0,20):
-    y_cord = centerY+(y*((radius*2)+5))
+    y_cord = centerY+(y*((radius*2)+margin))
     for x in range(0, 12):
-        x_cord = centerX+(x*((radius*2)+5))
-        f.write(f'\t<circle cx="{x_cord}" cy="{y_cord}" r="{radius}" style="{style}"/>\n')
+        x_cord = centerX+(x*((radius*2)+margin))
+        f.write(f'\t<circle cx="{x_cord}in" cy="{y_cord}in" r="{radius}in" style="{style}"/>\n')
 
 f.write('</svg>')
 f.close()
