62d47c79496c0260738954560d6693e163a90942
carlynorama <carlynorama@gmail.com>
2020-07-15 12:15:43 -0700

    radial line burst at center of page

--- radial_lines.py
+++ radial_lines.py
@@ -2,7 +2,7 @@ import math
 
 width_in = 8.5
 height_in = 11
-cicles_per_revolution = 36
+cicles_per_revolution = 4
 degree_step = int(360/cicles_per_revolution)
 radial_offset = 0
 offsetX = 0.25
@@ -10,7 +10,7 @@ offsetY = 0.25
 centerX = width_in/2
 centerY = height_in/2
 ext_radius = ((width_in- offsetX)/2)/2
-int_radius = 1
+int_radius = 0
 radius = ext_radius - int_radius
 x_cord = centerX
 y_cord = centerY
@@ -25,7 +25,7 @@ def line_at_angle(x, y, my_radius, angle):
 #radius = 0.015625#1/64th
 
 save_file_directory = "papers"
-save_file_name = "toroid_" + str(cicles_per_revolution) + "_per_cicle_" + str(int(ext_radius)) + "_ext_" + str(int(int_radius)) + "_int"
+save_file_name = "radial_lines_" + str(cicles_per_revolution) + "_per_cicle_" + str(int(ext_radius)) + "_ext_" + str(int(int_radius)) + "_int"
 save_file_full_path = save_file_directory + "/" + save_file_name
 
 trace_style = "stroke: rgb(200,200,200); fill: none;"
