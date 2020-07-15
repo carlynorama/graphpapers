5780d8d5ef195a1caa6029db97de82e96b0ab731
carlynorama <carlynorama@gmail.com>
2020-07-15 15:26:14 -0700

    polygon fixed

--- polygon.py
+++ polygon.py
@@ -2,7 +2,7 @@ import math
 
 width_in = 8.5
 height_in = 11
-number_verts = 5
+number_verts = 6
 radial_offset = math.radians(-90)
 offsetX = 0.25
 offsetY = 0.25
@@ -15,18 +15,34 @@ x_cord = centerX
 y_cord = centerY
 
 
-def line_at_angle(x, y, my_radius, angle_radians):
+def get_vertex(x, y, my_radius, angle_radians):
     end_x = math.cos(angle_radians)*my_radius + x
     end_y = math.sin(angle_radians)*my_radius + y
-    f.write(f'\t<line x1="{x}in" y1="{y}in" x2="{end_x}in" y2="{end_y}in" style="{trace_style}"/>\n')
+    return (end_x, end_y)
 
 def polygon(center_x, center_y, ext_radius, number_verts):
     degree_step = float(360/number_verts)
+    my_points = []
     for a in range(0, number_verts):
         radians = math.radians(a*degree_step) + radial_offset
-        line_at_angle(center_x, center_y, radius, radians)
+        my_points.append(get_vertex(center_x, center_y, radius, radians))
+    start_x = my_points[0][0]
+    start_y = my_points[0][1]
+    for i in range(1,number_verts):
+        end_x = my_points[i][0]
+        end_y = my_points[i][1]
+        f.write(f'\t<line x1="{start_x}in" y1="{start_y}in" x2="{end_x}in" y2="{end_y}in" style="{trace_style}"/>\n')
+        start_x = end_x
+        start_y = end_y
+    f.write(f'\t<line x1="{start_x}in" y1="{start_y}in" x2="{my_points[0][0]}in" y2="{my_points[0][1]}in" style="{trace_style}"/>\n')
 
 
+def get_poly_points_string(points_array):
+    points_string = " ".join(map(clean_tup, points_array))
+    return points_string
+
+def clean_tup(tuple):
+    return '{0},{1}'.format(*tuple)
 
 #radius = 0.015625#1/64th
 
