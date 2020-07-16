6fc4196347607fd9a6381a2c56437865c57baaee
carlynorama <carlynorama@gmail.com>
2020-07-15 17:13:55 -0700

    add close packing

--- grid_circles.py
+++ grid_circles.py
@@ -4,10 +4,14 @@ divisions_per_inch = 5
 offsetX = 0
 offsetY = 0
 padding = 0.015625
+tight_packed = True
 #radius = 0.015625#1/64th
 
 save_file_directory = "papers"
-save_file_name = "grid_circles_" + str(divisions_per_inch) + "_per_inch"
+if tight_packed:
+    save_file_name = "packed_circles_" + str(divisions_per_inch) + "_per_inch"
+else:
+    save_file_name = "grid_circles_" + str(divisions_per_inch) + "_per_inch"
 save_file_full_path = save_file_directory + "/" + save_file_name
 #margin = 0.5
 #spacing = radius*2 + margin
@@ -23,10 +27,12 @@ f.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
 f.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n')
 f.write(f'<svg width="{width_in}in" height="{height_in}in"  xmlns="http://www.w3.org/2000/svg">\n')
 
-for y in range(1,y_count):
-    y_cord = offsetY+(y*(y_spacing))
-    for x in range(1, x_count):
-        x_cord = offsetX+(x*(x_spacing))
+for row in range(1,y_count):
+    y_cord = offsetY+(row*(y_spacing))
+    for col in range(1, x_count):
+        x_cord = offsetX+(col*(x_spacing))
+        if tight_packed:
+            x_cord = x_cord + (row%2 * x_spacing/2)
         f.write(f'\t<circle cx="{x_cord}in" cy="{y_cord}in" r="{radius}in" style="{trace_style}"/>\n')
 
 f.write('</svg>')
